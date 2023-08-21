a1b46c181c7bed63a317be44db76a218f0a4caf2
Victor <victor.lecardonnel@hotmail.fr>
2023-08-21 19:00:07 +0100

    feat: added wait for shares to be purchased before sniping.

--- src/ethereum/contract.rs
+++ src/ethereum/contract.rs
@@ -1,3 +1,4 @@
+use std::thread;
 use ethers::core::types::{Address, U256};
 use ethers::prelude::TransactionReceipt;
 use crate::ethereum::config::WalletConfig;
@@ -5,7 +6,13 @@ use crate::ethereum::config::WalletConfig;
 pub async fn call_buy_shares(config: WalletConfig, buy_address: Address, amount: U256) -> TransactionReceipt {
     let contract = config.contract.clone();
 
-    let transaction_value: U256 = contract.get_buy_price(buy_address.clone(), amount.clone()).await.unwrap();
+    let mut transaction_value: U256 = contract.get_buy_price_after_fee(buy_address.clone(), amount.clone()).await.unwrap();
+
+    while transaction_value == U256::zero() {
+        log::info!("Waiting for user to buy shares...");
+        transaction_value = contract.get_buy_price(buy_address.clone(), amount.clone()).await.unwrap();
+        thread::sleep(std::time::Duration::from_millis(500));
+    }
 
     log::info!("Current price of shares: {}", &transaction_value);
 
@@ -41,6 +48,7 @@ pub async fn call_buy_shares(config: WalletConfig, buy_address: Address, amount:
     transaction
 }
 
+// TODO: remove expect
 pub async fn get_owned_shares(config: WalletConfig, address: Address) -> U256 {
     config.contract.clone().shares_balance(address, config.wallet_address.clone())
         .call()
