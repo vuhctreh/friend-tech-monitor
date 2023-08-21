7fff17d1ab652013fa971ce4181c4de3da85a0c9
Victor <victor.lecardonnel@hotmail.fr>
2023-08-21 19:02:42 +0100

    misc: ?

--- src/ethereum/sniper.rs
+++ src/ethereum/sniper.rs
@@ -1,9 +1,13 @@
 use ethers::types::Address;
+use ethers::types::U256;
 use crate::ethereum::config::WalletConfig;
 use crate::ethereum::contract::{call_buy_shares, get_owned_shares};
 
-pub async fn snipe(config: WalletConfig, address: Address) -> Result<(), String> {
-    let  receipt = call_buy_shares(config.clone(), address, 1.into()).await;
+pub async fn snipe(config: &WalletConfig, address: Address, amount: &u64) -> Result<(), String> {
+
+    let amount = U256::from(amount.clone());
+
+    let  receipt = call_buy_shares(config.clone(), address, amount).await;
 
     log::debug!("{receipt:?}");
 
@@ -18,7 +22,7 @@ pub async fn snipe(config: WalletConfig, address: Address) -> Result<(), String>
         }
     }
 
-    println!("Shares owned: {}", get_owned_shares(config.clone(), address).await);
+    log::info!("Shares owned: {}", get_owned_shares(config.clone(), address).await);
 
     Ok(())
 }
