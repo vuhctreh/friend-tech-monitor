9f5b92d75504f51b3158fc3d168dc46fa2e14659
Victor <victor.lecardonnel@hotmail.fr>
2023-08-21 19:01:17 +0100

    chores: some TODOS and updated loop in main

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,15 @@ mod ethereum;
 mod monitor;
 
 // TODO: make sniper and webhook parallel.
+// TODO: add sniper limit pricing and retries
+// TODO: add take profit
+// TODO: add inventory management
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     SimpleLogger::new().init().unwrap();
 
     dotenv().expect("ERROR: Could not load .env file.");
-
-    let webhook_url: String = std::env::var("WEBHOOK_URL")
-        .expect("ERROR: WEBHOOK_URL env has not been set.");
 
     let delay = std::env::var("DELAY")
         .expect("ERROR: DELAY in .env was not set")
@@ -29,9 +30,14 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let config: WalletConfig = WalletConfig::new().await;
 
-    monitor(client, config, &webhook_url, delay).await?;
+    loop {
+        let res =  monitor(client.clone(), config.clone(), delay).await;
 
-    Ok(())
+        match res {
+            Ok(_) => {},
+            Err(e) => log::error!("{:?}", e)
+        }
+    }
 }
 
 #[tokio::test]
