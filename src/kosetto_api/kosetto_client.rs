67f6978efa2c732994f636b9a409cbead1f78196
Victor <victor.lecardonnel@hotmail.fr>
2023-08-21 19:00:53 +0100

    misc: some logs

--- src/kosetto_api/kosetto_client.rs
+++ src/kosetto_api/kosetto_client.rs
@@ -4,12 +4,16 @@ use reqwest::StatusCode;
 
 pub async fn search_user(client: &Client, user: &String) -> Result<KosettoResponse, StatusCode> {
 
+    log::info!("Searching for user: {}", user);
+
     let url: String = format!("{}{}", std::env::var("KOSETTO_URL").unwrap(), user);
 
     let resp = client.get(url)
         .send()
         .await
         .expect("ERROR: Failed to get user from Kosetto API.");
+
+    log::info!("Got response.");
 
     // TODO: Handle response error properly
     match resp.status() {
@@ -19,11 +23,11 @@ pub async fn search_user(client: &Client, user: &String) -> Result<KosettoRespon
     }
 }
 
-pub async fn find_user_in_search(user_info: &KosettoResponse, monitor_target: String) -> Option<&User> {
+pub fn find_user_in_search(user_info: &KosettoResponse, monitor_target: &String) -> Option<User> {
     for user in user_info.users.iter() {
         if user.twitter_username == monitor_target.clone() {
             log::info!("Found user {}.", monitor_target);
-            return Some(user);
+            return Some(user.clone());
         } else {
             log::info!("{} did not match monitor target.", user.twitter_username.clone());
         }
