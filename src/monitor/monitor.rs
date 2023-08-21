a19db0b724183af991786efff22efefcdffc02ab
Victor <victor.lecardonnel@hotmail.fr>
2023-08-21 19:02:30 +0100

    refactor: cleaned monitor.rs (split into 2 functions)

--- src/monitor/monitor.rs
+++ src/monitor/monitor.rs
@@ -1,90 +1,109 @@
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 use ethers::types::Address;
-use reqwest::{Client, Response, StatusCode};
+use reqwest::{Client, Error, Response, StatusCode};
 use crate::discord_utils::types::Webhook;
 use crate::discord_utils::webhook_utils::{post_webhook, prepare_webhook};
 use crate::ethereum;
 use crate::ethereum::config::WalletConfig;
-use crate::io_utils::json_loader::load_monitor_list;
+use crate::io_utils::json_loader::{load_monitor_list, write_monitor_list};
 use crate::kosetto_api::kosetto_client;
 use crate::kosetto_api::kosetto_client::find_user_in_search;
-use crate::kosetto_api::types::KosettoResponse;
+use crate::kosetto_api::types::{KosettoResponse};
 
-pub async fn monitor(client: Client, config: WalletConfig, webhook_url: &String, delay: u64) -> Result<String, String> {
-    let mut monitor_list: Vec<String> = load_monitor_list().monitor;
+// TODO: clean this up
+pub async fn monitor(client: Client, config: WalletConfig, delay: u64) -> Result<String, String> {
 
-    loop {
-        for target in monitor_list.clone().iter() {
+    let monitor_map: HashMap<String, u64> = load_monitor_list();
 
-            log::info!("Beginning monitor for: {}", target);
+    if load_monitor_list().len() == 0 {
+        panic!("Monitor list is empty");
+    }
 
-            let monitor_target = &target.clone();
+    let mut new_map: HashMap<String, u64> = HashMap::new();
 
-            let user_info: Result<KosettoResponse, StatusCode> = kosetto_client::search_user(&client, monitor_target)
-                .await;
+    for (key, value) in monitor_map.iter() {
+        log::info!("Beginning monitor for: {}", key);
+        let monitor_target = &key.clone();
 
-            match user_info {
-                // If user info is returned from Kosetto API
-                Ok(user_info) => {
+        let user_info: Result<KosettoResponse, StatusCode> = kosetto_client::search_user(&client, monitor_target)
+            .await;
 
-                    // Search for monitored user in endpoint -> returns None if not found
-                    let res = find_user_in_search(&user_info, monitor_target.clone()).await;
+        match user_info {
+            Ok(user_info) => {
+                parse_response(config.clone(), user_info, key.clone(), value.clone(), client.clone()).await;
+            }
+            Err(StatusCode::NOT_FOUND) => {
+                log::info!("No users returned from search.");
+                new_map.insert(monitor_target.clone(), *value);
+            }
+            Err(e) => {
+                log::error!("User search failed with status code: {}", e);
+                new_map.insert(monitor_target.clone(), *value);
+            }
+        }
 
-                    match res {
-                        // If exact match
-                        Some(x) => {
+        thread::sleep(Duration::from_secs(2));
+    }
 
-                            match x.address.parse::<Address>() {
-                                Ok(address) => {
-                                    ethereum::sniper::snipe(config.clone(), address).await.unwrap_or(log::error!("Failed to snipe {}", address));
-                                },
-                                Err(e) => {
-                                    log::error!("Could not parse address: {}", e);
-                                }
-                            }
+    write_monitor_list(new_map).expect("TODO: panic message");
 
-                            // Prepare & send webhook
-                            let webhook: Webhook = prepare_webhook(x);
-                            let resp: Result<Response, reqwest::Error> = post_webhook(&client, &webhook_url, &webhook).await;
+    log::info!("Sleeping for: {}s", delay);
+    thread::sleep(Duration::from_secs(delay));
 
-                            match resp {
-                                // Sent webhook
-                                Ok(resp) => {
-                                    // Ok
-                                    if resp.status() == StatusCode::OK || resp.status() == StatusCode::NO_CONTENT {
-                                        log::info!("Successfully sent webhook for: {} with status code: {}", target, resp.status());
-                                    } else {
-                                        // Webhook sent but not successful
-                                        log::warn!("Failed to send webhook for: {} with status code: {}", target, resp.status());
-                                    }
-                                }
-                                // Failed to send webhook
-                                Err(e) => {
-                                    log::warn!("Failed to send webhook for: {}", target);
-                                    log::error!("{:?}", e);
-                                }
-                            }
-                            // Remove target from monitor list
-                            <Vec<String> as AsMut<Vec<String>>>::as_mut(&mut monitor_list).retain(|x| x != monitor_target);
-                        }
-                        None => {
-                            log::info!("No match found for: {}", monitor_target);
+    Ok("h".to_string())
+}
+
+// TODO: clean this up
+// Errors: 1 =
+async fn parse_response(config: WalletConfig, response: KosettoResponse, target: String, amount: u64, client: Client) -> Result<(), Error> {
+    // Search for monitored user in endpoint -> returns None if not found
+    let res = find_user_in_search(&response, &target);
+
+    match res {
+        Some(matching_user) => {
+            match matching_user.address.parse::<Address>() {
+                Ok(address) => {
+                    let snipe_result = ethereum::sniper::snipe(&config, address, &amount).await;
+
+                    match snipe_result {
+                        Ok(_) => {}
+                        Err(e) => {
+                            log::error!("Sniper failed with error: {}", e);
                         }
                     }
                 },
                 Err(e) => {
-                    log::error!("status code => {}", e);
+                    log::error!("Could not parse address: {}", e);
                 }
             }
 
-            log::info!("Sleeping for 1 second...");
-            thread::sleep(Duration::from_secs(1));
-        }
+            // Prepare & send webhook
+            let webhook: Webhook = prepare_webhook(matching_user);
+            let resp: Result<Response, Error> = post_webhook(&client,  &webhook).await;
 
-        log::info!("Finished monitoring all targets. Sleeping for 10 seconds...");
-        thread::sleep(Duration::from_secs(delay));
-        log::info!("Beginning new monitoring cycle.");
-        println!("--------------------------------");
+            match resp {
+                Ok(response) => {
+                    if response.status() == StatusCode::OK || response.status() == StatusCode::NO_CONTENT {
+                        log::info!("Successfully sent webhook for: {} with status code: {}", &target, response.status());
+                    } else {
+                        // Webhook sent but not successful
+                        log::warn!("Failed to send webhook for: {} with status code: {}", &target, response.status());
+                    }
+                }
+                // Failed to send webhook
+                Err(e) => {
+                    log::warn!("Failed to send webhook for: {}", &target);
+                    log::error!("{:?}", e);
+                }
+            }
+
+        }
+        None => {
+            log::info!("No match found for: {}", &target);
+        }
     }
+
+    Ok(())
 }
