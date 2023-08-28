9c91b74da439edb60349aca0d3a37515c4ea64dc
Victor <victor.lecardonnel@hotmail.fr>
2023-08-28 23:58:39 +0100

    misc: allowed unused

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@
 //! - `friend.tech contract:` <https://basescan.org/address/0xcf205808ed36593aa40a44f10c7f7c2f67d4a4d4#readContract>
 //! - `base:` <https://base.org/>
 
+
+#![allow(unused)]
 use std::error::Error;
 use std::thread;
 use std::time::Duration;
