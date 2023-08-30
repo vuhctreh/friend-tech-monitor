38c98b79d32964d46f8175f93096d4998b0e44c5
Victor <victor.lecardonnel@hotmail.fr>
2023-08-30 16:59:51 +0100

    fix & refacotr: fixed double posting by moving block retrieval outside the monitor + created resolve_user_by_address for better error handling in thread

--- src/monitor_v2/monitor_v2.rs
+++ src/monitor_v2/monitor_v2.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
 use std::str::FromStr;
+use std::sync::Arc;
 use std::thread;
 use ethers::abi::AbiDecode;
+use ethers::prelude::k256::elliptic_curve::rand_core::block::BlockRngCore;
 use ethers::providers::{Http, Middleware, Provider};
 use ethers::types::{Address, Block, BlockNumber, Transaction, U256};
 use eyre::{Result};
@@ -16,9 +18,58 @@ use crate::kosetto_api::types::ExactUser;
 
 const ADDRESS: &str = "0xcf205808ed36593aa40a44f10c7f7c2f67d4a4d4";
 
-pub async fn get_previous_block_txs(provider: &Provider<Http>) -> Result<Option<Vec<Transaction>>> {
-    let block_number: BlockNumber = BlockNumber::from(provider.get_block_number().await?);
+// TODO: delays
+pub async fn monitor_v2(commons: &WalletCommons, block_number: BlockNumber) -> Result<()> {
+    let provider: Provider<Http> = commons.provider.clone();
 
+    // let monitor_map: HashMap<String, u64> = load_monitor_list()?;
+    //
+    // if monitor_map.len() == 0usize {
+    //     log::warn!("monitor.json is empty. Bot will continue running in case there are snipes ongoing.");
+    // }
+    //
+    // let mut _new_map: HashMap<String, u64> = HashMap::new();
+
+    let previous_block_txs: Option<Vec<Transaction>> = get_previous_block_txs(&provider,  block_number).await?;
+
+    let filtered_transactions: Vec<BuySharesCall> = match previous_block_txs {
+        Some(txs) => {
+            filter_signup_txs(txs)?
+        },
+        None => vec![]
+    };
+
+    if filtered_transactions.len() > 0 {
+        log::info!("Buys in block: {}", filtered_transactions.len());
+
+        for tx in filtered_transactions {
+            tokio::spawn(async move {
+                let thread_client: Client = Client::new();
+
+                let user_data: Result<ExactUser> = resolve_user_by_address(&thread_client, tx.shares_subject.clone()).await;
+
+                match user_data {
+                    Ok(data) => {
+                        // tokio::spawn(async move {});
+                        let webhook: Webhook = prepare_user_signup_embed(data);
+                        let _resp: Response = post_webhook(&thread_client,  &webhook).await.unwrap();
+                    }
+                    Err(e) => {
+                        log::error!("Failed to resolve user with address: {}, {}", tx.clone(), e);
+                    }
+                }
+            });
+            // if monitor_map.contains_key(&user_data.address) {
+            //     let webhook: Webhook = prepare_user_signup_embed(matching_user);
+            //     let resp: Response = post_webhook(&client,  &webhook).await?;
+            // }
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn get_previous_block_txs(provider: &Provider<Http>, block_number: BlockNumber) -> Result<Option<Vec<Transaction>>> {
     let transactions: Option<Block<Transaction>> = provider.get_block_with_txs(block_number).await?;
 
     match transactions {
@@ -54,87 +105,29 @@ pub fn filter_signup_txs(txs: Vec<Transaction>) -> Result<Vec<BuySharesCall>> {
     Ok(filtered_txs)
 }
 
-pub async fn monitor_v2(commons: WalletCommons) -> Result<()> {
-    let provider: Provider<Http> = commons.provider.clone();
+pub async fn resolve_user_by_address(client: &Client, address: Address) -> Result<ExactUser> {
+    let mut user: Response = get_user_by_address(client, address).await?;
 
-    let monitor_map: HashMap<String, u64> = load_monitor_list()?;
-
-    if monitor_map.len() == 0usize {
-        log::warn!("monitor.json is empty. Bot will continue running in case there are snipes ongoing.");
-    }
-
-    let mut _new_map: HashMap<String, u64> = HashMap::new();
-
-    let previous_block_txs: Option<Vec<Transaction>> = get_previous_block_txs(&provider).await?;
-
-    let filtered_transactions: Option<Vec<BuySharesCall>> = match previous_block_txs {
-        Some(txs) => {
-            Some(filter_signup_txs(txs)?)
-        },
-        None => None,
-    };
-
-    match filtered_transactions {
-        Some(transactions) => {
-            for tx in transactions {
-                // TODO: error handling in here
-                tokio::spawn(async move {
-                    let thread_client: Client = Client::new();
-
-                    let mut user: Response = get_user_by_address(&thread_client, tx.shares_subject).await.unwrap();
-
-                    match user.status() {
-                        StatusCode::OK => {}
-                        StatusCode::BAD_GATEWAY => {
-                            while user.status() == StatusCode::BAD_GATEWAY {
-                                log::warn!("502 error getting user. Retrying...");
-                                user = get_user_by_address(&thread_client, tx.shares_subject).await.unwrap();
-                                thread::sleep(std::time::Duration::from_millis(350));
-                            }
-                        }
-                        StatusCode::NOT_FOUND => {
-                            while user.status() == StatusCode::NOT_FOUND {
-                                log::warn!("404 error getting user. Retrying...");
-                                user = get_user_by_address(&thread_client, tx.shares_subject).await.unwrap();
-                                thread::sleep(std::time::Duration::from_millis(350));
-                            }
-                        }
-                        _ => {
-                            log::error!("Error: {}", user.status());
-                        }
-                    }
-
-                    let user_data: ExactUser = serde_json::from_str(&user.text().await.unwrap()).unwrap();
-
-                    thread::sleep(std::time::Duration::from_millis(150));
-
-                    let webhook: Webhook = prepare_user_signup_embed(user_data);
-                    let _resp: Response = post_webhook(&thread_client,  &webhook).await.unwrap();
-                });
-                // if monitor_map.contains_key(&user_data.address) {
-                //     let webhook: Webhook = prepare_user_signup_embed(matching_user);
-                //     let resp: Response = post_webhook(&client,  &webhook).await?;
-                // }
+    match user.status() {
+        StatusCode::OK => {}
+        StatusCode::BAD_GATEWAY => {
+            while user.status() == StatusCode::BAD_GATEWAY {
+                log::warn!("502 error getting user. Retrying...");
+                user = get_user_by_address(&client, address).await?;
+                thread::sleep(std::time::Duration::from_millis(350));
             }
         }
-        None => {}
+        StatusCode::NOT_FOUND => {
+            while user.status() == StatusCode::NOT_FOUND {
+                log::warn!("404 error getting user. Retrying...");
+                user = get_user_by_address(client, address).await?;
+                thread::sleep(std::time::Duration::from_millis(350));
+            }
+        }
+        _ => {
+            log::error!("Error: {}", user.status());
+        }
     }
 
-    Ok(())
-}
-
-
-#[tokio::test]
-async fn test_get_previous_block_txs() -> Result<()> {
-    let provider = Provider::<Http>::try_from("https://developer-access-mainnet.base.org")?;
-    let previous_block_txs = get_previous_block_txs(&provider).await?;
-
-    match previous_block_txs {
-        Some(txs) => {
-            println!("{:?}", filter_signup_txs(txs)?);
-        },
-        None => {}
-    }
-
-    Ok(())
+    Ok(serde_json::from_str(&user.text().await?)?)
 }
