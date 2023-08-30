b8441d9064a57905b57bd386169b7522c94f1ca3
Victor <victor.lecardonnel@hotmail.fr>
2023-08-30 16:56:35 +0100

    refactor: moved URL to a const and

--- src/kosetto_api/kosetto_client.rs
+++ src/kosetto_api/kosetto_client.rs
@@ -4,13 +4,15 @@ use ethers::types::Address;
 use reqwest::{Client, Response};
 use eyre::Result;
 
+const URL: &str = "https://prod-api.kosetto.com/users/";
+
 /// Gets a user by their address. This endpoint does not need a token.
 pub async fn get_user_by_address(client: &Client, address: Address) -> Result<Response> {
     log::info!("Getting user info for address: {}", address);
 
     // Read this for explanation on formatting:
     // https://stackoverflow.com/questions/57350082/to-convert-a-ethereum-typesh256-to-string-in-rust
-    let url: String = format!("https://prod-api.kosetto.com/users/{:#x}", address);
+    let url: String = format!("{}{:#x}", URL, address);
 
     let resp = client.get(url)
        .send()
