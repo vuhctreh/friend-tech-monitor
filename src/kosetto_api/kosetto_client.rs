62365704f2a3631b8a3b800c670ec41ed4ea7af9
Victor <victor.lecardonnel@hotmail.fr>
2023-09-09 14:32:56 +0100

    refactor: uses Arc<Client> from ApplicationCommons

--- src/kosetto_api/kosetto_client.rs
+++ src/kosetto_api/kosetto_client.rs
@@ -1,5 +1,6 @@
 //! Implementations for calling the Kosetto (friend.tech) API.
 
+use std::sync::Arc;
 use ethers::types::Address;
 use reqwest::{Client, Response};
 use eyre::Result;
@@ -7,7 +8,7 @@ use eyre::Result;
 const URL: &str = "https://prod-api.kosetto.com/users/";
 
 /// Gets a user by their address. This endpoint does not need a token.
-pub async fn get_user_by_address(client: &Client, address: Address) -> Result<Response> {
+pub async fn get_user_by_address(client: Arc<Client>, address: Address) -> Result<Response> {
     log::info!("Getting user info for address: {}", address);
 
     // Read this for explanation on formatting:
